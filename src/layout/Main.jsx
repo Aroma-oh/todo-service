78031cc2a97430ba0b104509ba300348f2a0bb58
Aroma-oh <115550622+Aroma-oh@users.noreply.github.com>
2023-04-17 08:56:35 +0900

    style: 필요없는 코드 제거

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,46 +1,44 @@
-import styled from "styled-components";
-import { useSelector, useDispatch } from "react-redux";
-import { Post } from "../components/Post";
+import styled from 'styled-components';
+import { useSelector, useDispatch } from 'react-redux';
+import { Post } from '../components/Post';
 
 export const MainContainer = styled.div`
-    display: flex;
-    flex-direction: row;
-    justify-content: flex-start;
-    align-items: flex-start;
-    align-content: flex-start;
-    flex-wrap: wrap;
-    padding: 24px 24px;
-    gap: 4px;
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: flex-start;
+  align-content: flex-start;
+  flex-wrap: wrap;
+  padding: 24px 24px;
+  gap: 4px;
 
-    position: relative;
-    width: 85vw;
-    height: 100vh;
-    left: 260px;
-    top: 100px;
+  position: relative;
+  width: 85vw;
+  height: 100vh;
+  left: 260px;
+  top: 100px;
 
-    border-radius: 16px 0px 0px 0px;
-    background-color: ${(props) => props.theme.colors.mainBgColor};
+  border-radius: 16px 0px 0px 0px;
+  background-color: ${(props) => props.theme.colors.mainBgColor};
 `;
 
 export const Main = () => {
-    const dispatch = useDispatch();
+  const data = useSelector((state) => state.todos.todos);
+  const hideDoneTask = useSelector((state) => state.event.hideDoneTask);
 
-    const data = useSelector((state) => state.todos.todos);
-    const hideDoneTask = useSelector((state) => state.event.hideDoneTask);
-
-    return (
-        <>
-            <MainContainer>
-                {data
-                    .filter((el) => {
-                        if (hideDoneTask) return el.done === false;
-                        else return el;
-                    })
-                    .map((el) => {
-                        return <Post {...el} />;
-                    })}
-            </MainContainer>
-            {/* <CreateModal type="edit" /> */}
-        </>
-    );
+  return (
+    <>
+      <MainContainer>
+        {data
+          .filter((el) => {
+            if (hideDoneTask) return el.done === false;
+            else return el;
+          })
+          .map((el) => {
+            return <Post {...el} />;
+          })}
+      </MainContainer>
+      {/* <CreateModal type="edit" /> */}
+    </>
+  );
 };
